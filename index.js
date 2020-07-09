8eccb3c08bf4993869c6ac10b9f4c375eaec2fd1
zhangjiewei <1525456439@qq.com>
2020-07-09 16:51:25 +0800

    login and register 初步完成

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 //通过内置中间件解析请求体内容
 app.use(express.urlencoded({extended:true}));
 
-app.post('/register',(request,response) => {
+app.post('/register',async (request,response) => {
   /*
   * 1.接收请求数据
   * 2.验证请求数据的格式
@@ -26,20 +26,74 @@ app.post('/register',(request,response) => {
   const {userName,password,repeatPassword,email} = request.body;
   //2.验证请求数据的格式
   const userNameReg = /^[A-Za-z0-9_]{5,16}$/;
-  const passwordReg = /^[[A-Za-z0-9_]{6,20}]$/;
-  const emailReg = /^[A-Za-z0-9_]{3,8}@[A-Za-z0-9_]{2,6}\.com$/;
+  const passwordReg = /^[[A-Za-z0-9_]{6,20}$/;
+  const emailReg = /^[A-Za-z0-9_]{3,11}@[A-Za-z0-9_]{2,6}\.com$/;
 
-  if(userNameReg(userName)){
-
+  if(!userNameReg.test(userName)){
+      response.send('用户名可以包含英文字母、数字、下划线，长度为5-16');
+      return;
+  }else if(!passwordReg.test(password)){
+      console.log(request.body);
+      response.send('密码可以包含英文字母、数字、下划线，长度为6-20');
+      return;
+  }else if(password !== repeatPassword){
+      response.send('两次密码输入不一致，请重新输入');
+      console.log(response.body);
+      return;
+  }else if(!emailReg.test(email)){
+      response.send('邮箱格式不正确');
+      return;
   }
-  //3.将数据存入数据库中保存
+  //3.查询数据库用户名是否已存在，若是存在返回，若是不存在执行下一步
+  try{
+    const user = await users.findOne({userName});
+    if(user){
+      //用户名存在
+      response.send('用户已存在，请重新注册');
+    }else {
+      //成功在数据库中保存
+      await users.create({userName,password,email});
+      response.send('注册成功！')
+    }
 
-  console.log(request.body);
-  response.send('register');
+  }catch (e){
+    console.log(e);
+    response.send('网络不稳定，请刷新重试！！')
+  }
 });
-app.post('/login',(request,response) => {
-  console.log(request.body);
-  response.send('login');
+app.post('/login',async(request,response) => {
+  /*
+  * 1.获取数据
+  * 2.查看数据的格式是否符合要求
+  * 3.查看数据库中是否存在该用户
+  * 4.对比用户名和密码
+  * 5.成功登陆
+  * */
+  const {userName,password} = request.body;
+  console.log(userName,password);
+  const userNameReg = /^[A-Za-z0-9_]{5,16}$/;
+  const passwordReg = /^[[A-Za-z0-9_]{6,20}$/;
+  if(!userNameReg.test(userName)){
+    response.send('用户名或密码错误！！');
+    return;
+  }else if (!passwordReg.test(password)){
+    response.send('用户名或密码错误！！')
+    return;
+  }
+  try {
+    const user = await users.findOne({userName});
+    console.log(user);
+    if (user.userName == userName && user.password == password) {
+      response.send('登陆成功');
+    } else {
+      response.send('用户名错误！！！')
+    }
+  }catch(e){
+    console.log(e);
+    response.send('网络不稳定，请刷新重试')
+  }
+
+
 })
 
 
